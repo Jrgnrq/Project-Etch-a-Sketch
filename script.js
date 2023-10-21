f1abc3207f4914cb6b4c6da504c0f17c563bdad3
Jrgnrq <118330583+Jrgnrq@users.noreply.github.com>
2023-10-21 14:56:52 -0600

    hovering function developed, and radio buttons section added

--- script.js
+++ script.js
@@ -10,11 +10,49 @@ function sketchTable(columns){
     while (round < count) {
         var gridTable = document.createElement("div");
         gridTable.className = "square";
+        gridTable.setAttribute("id", `square${round}` )
     sketch.appendChild(gridTable);
         round ++ ;
     }
     sketch.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
     sketch.style.gridTemplateRows = `repeat(${columns}, 1fr)`;
 }
-
-sketchTable(10)
+//function that make the hovering
+function hovering(){        
+    var squareElements = document.querySelectorAll(".square");
+    
+        squareElements.forEach(function(div) {
+        div.addEventListener("mouseenter", function() {
+        div.style.backgroundColor = 'lightcoral';
+            });
+            })
+    }
+//function that select the color background
+//code neede for the ratio buttons works 
+var smallSketch = document.getElementById("smallSkech");
+    smallSketch.addEventListener("click", function() {
+        var squares = document.querySelectorAll(".square");
+            squares.forEach(function(square) {
+            sketch.removeChild(square);
+            });
+        sketchTable(10);
+        hovering();
+    });
+var mediumSketch = document.getElementById("mediumSkech");
+    mediumSketch.addEventListener("click", function() {
+        var squares = document.querySelectorAll(".square");
+            squares.forEach(function(square) {
+            sketch.removeChild(square);
+            });
+        sketchTable(20);
+        hovering();
+    });
+var bigSketch = document.getElementById("bigSkech");
+    bigSketch.addEventListener("click", function() {
+        var squares = document.querySelectorAll(".square");
+            squares.forEach(function(square) {
+            sketch.removeChild(square);
+            });           
+        sketchTable(50);
+        hovering();
+    });
