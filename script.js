c8e3a10053ff271723ba2e36dc651502a9e0ffe6
Jrgnrq <118330583+Jrgnrq@users.noreply.github.com>
2023-10-21 23:24:19 -0600

    function that select the color background developed

--- script.js
+++ script.js
@@ -17,17 +17,43 @@ function sketchTable(columns){
     sketch.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
     sketch.style.gridTemplateRows = `repeat(${columns}, 1fr)`;
 }
+function getRandomColor(){
+    const LIMIT_VALUE = 256;
+    let red = getRandomNumber (LIMIT_VALUE);
+    let green = getRandomNumber (LIMIT_VALUE);
+    let blue = getRandomNumber (LIMIT_VALUE);
+    return 'rgb('+red+','+green+','+blue+')';
+}
+function getRandomNumber(limitValue){
+    let randomNumber = Math.random();
+    randomNumber = randomNumber*limitValue;
+    randomNumber = Math.floor(randomNumber);
+    return randomNumber;
+}
+
+//function that select the color background
+function selectBackgroundColor(colorType){
+    switch (colorType) {
+        case "classicColor":
+            return "black";
+        case "randomColor":
+            return getRandomColor();
+        default:
+            return "white"; // Default color
+    }
+}
 //function that make the hovering
 function hovering(){        
     var squareElements = document.querySelectorAll(".square");
     
         squareElements.forEach(function(div) {
         div.addEventListener("mouseenter", function() {
-        div.style.backgroundColor = 'lightcoral';
+        var colorType = document.querySelector('input[name="colorType"]:checked').value;
+        div.style.backgroundColor = selectBackgroundColor(colorType);
             });
             })
-    }
-//function that select the color background
+}
+
 //code neede for the ratio buttons works 
 var smallSketch = document.getElementById("smallSkech");
     smallSketch.addEventListener("click", function() {
@@ -35,7 +61,7 @@ var smallSketch = document.getElementById("smallSkech");
             squares.forEach(function(square) {
             sketch.removeChild(square);
             });
-        sketchTable(10);
+        sketchTable(20);
         hovering();
     });
 var mediumSketch = document.getElementById("mediumSkech");
@@ -44,15 +70,17 @@ var mediumSketch = document.getElementById("mediumSkech");
             squares.forEach(function(square) {
             sketch.removeChild(square);
             });
-        sketchTable(20);
+        sketchTable(50);
         hovering();
     });
+
+    
 var bigSketch = document.getElementById("bigSkech");
     bigSketch.addEventListener("click", function() {
         var squares = document.querySelectorAll(".square");
             squares.forEach(function(square) {
             sketch.removeChild(square);
             });           
-        sketchTable(50);
+        sketchTable(80);
         hovering();
     });
